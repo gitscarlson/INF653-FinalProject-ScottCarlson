6f1c870613d4235696a34fa36da44a8269cd9cd9
gitscarlson <scott73carlson@gmail.com>
2023-04-22 13:48:00 -0500

    updated allowedOrigins

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,12 +1,8 @@
-const whitelist = [
-    'https://inf653-finalproject-scottcarlson.scott73carlson.repl.co', 
-    'https://dazzling-snickerdoodle-777101.netlify.app/',
-    'http://127.0.0.1:5500', 
-    'http://localhost:3500'
-];
+const allowedOrigins = require('./allowedOrigins');
+
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
