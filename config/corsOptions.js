0418aed467c05975c7e604dfffc5dfdd756a079c
gitscarlson <scott73carlson@gmail.com>
2023-04-22 11:57:39 -0500

    cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,6 @@
 const whitelist = [
     'https://inf653-finalproject-scottcarlson.scott73carlson.repl.co', 
+    'https://dazzling-snickerdoodle-777101.netlify.app/',
     'http://127.0.0.1:5500', 
     'http://localhost:3500'
 ];
